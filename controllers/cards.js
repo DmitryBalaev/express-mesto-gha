95a5c13fa9072e278acfc3e3cf6b169f7e125f42
Dmitry Balaev <vitalij-borman@yandex.ru>
2023-06-02 00:08:18 +0300

    fix: 400 or 500

--- controllers/cards.js
+++ controllers/cards.js
@@ -47,10 +47,10 @@ const deleteCard = (req, res) => {
   Card.findByIdAndDelete(req.params.cardId)
     .orFail(new Error('NotValidId'))
     .then((deletedCard) => {
-      if (!deletedCard) {
-        res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
-      } else {
+      if (deletedCard) {
         res.send({ message: deletedCard });
+      } else {
+        res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
       }
     })
     .catch((err) => {
@@ -81,10 +81,10 @@ const removeLikeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
     .orFail(new Error('NotValidId'))
     .then((card) => {
-      if (!card) {
-        res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
-      } else {
+      if (card) {
         res.send({ data: card });
+      } else {
+        res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
       }
     })
     .catch((err) => {
