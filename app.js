f5046f9b1ee69dcc539fd29681786d989fa4b046
Dmitry Balaev <vitalij-borman@yandex.ru>
2023-06-13 15:09:09 +0300

    fix: обработка ошибок

--- app.js
+++ app.js
@@ -3,15 +3,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const { errors } = require('celebrate');
 
-const { login, createUser } = require('./controllers/users');
-const usersRouter = require('./routes/userRouter');
-const cardsRouter = require('./routes/cardRouter');
+const router = require('./routes');
 const { PORT, MONGO_DB } = require('./utils/config');
-const { NotFound } = require('./utils/responsesErrors/NotFound');
-const authMiddleware = require('./middlewares/auth');
 const { responseHandler } = require('./middlewares/responseHandler');
-const { validateLogin, validateRegistration } = require('./utils/validationDataConfig');
 
 const app = express();
 
@@ -33,24 +29,9 @@ app.use(helmet());
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '647756bd85d90546a56b9889',
-  };
+app.use(router);
 
-  next();
-});
-
-app.use(usersRouter, authMiddleware);
-app.use(cardsRouter, authMiddleware);
-
-app.use('/signin', validateLogin, login);
-app.use('/signup', validateRegistration, createUser);
-
-app.use('*', authMiddleware, (req, res, next) => {
-  next(new NotFound('Указан не существующий путь.'));
-});
-
+app.use(errors());
 app.use(responseHandler);
 
 app.listen(PORT, () => {
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,7 @@ const { SECRET } = require('../utils/config');
 const { Unauthorized } = require('../utils/responsesErrors/Unauthorized');
 
 const handleError = (req, res, next) => {
-  next(new Unauthorized('С токеном что-то не так.'));
+  next(new Unauthorized('С токеном что-то не так'));
 };
 
 // eslint-disable-next-line consistent-return
@@ -11,10 +11,14 @@ module.exports = function uathMiddleware(req, res, next) {
   const { authorization } = req.headers;
   let payload;
 
-  if (!authorization || !authorization.startsWith('Bearer ')) return handleError(req, res, next);
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return handleError(req, res, next);
+  }
   try {
     const token = req.headers.authorization.split(' ')[1];
-    if (!token) return handleError(req, res, next);
+    if (!token) {
+      return handleError(req, res, next);
+    }
     payload = jwt.verify(token, SECRET);
   } catch (err) {
     return handleError(req, res, next);
