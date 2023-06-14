dfb045caed6298d6682203dc092d521da8ae23c3
Dmitry Balaev <vitalij-borman@yandex.ru>
2023-06-14 13:26:16 +0300

    fix: config

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
-const { PORT, MONGO_DB } = require('./utils/config');
 const router = require('./routes');
 const { responseHandler } = require('./middlewares/responseHandler');
 
+const { PORT = 5500, MONGO_DB = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+
 const app = express();
+app.use(cookieParser());
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -25,7 +27,6 @@ mongoose.connect(MONGO_DB, {
   autoIndex: true,
 });
 
-app.use(cookieParser());
 app.use(limiter);
 app.use(helmet());
 
