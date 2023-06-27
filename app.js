e7f2d6eda0ac75dc16b8d49a9c0bec6e1ab3ff91
Dmitry Balaev <vitalij-borman@yandex.ru>
2023-06-27 15:59:14 +0300

    logger

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const router = require('./routes');
 const { responseHandler } = require('./middlewares/responseHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000, MONGO_DB = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
@@ -31,9 +32,11 @@ app.use(limiter);
 app.use(helmet());
 
 app.use(express.json());
+app.use(requestLogger);
 
 app.use(router);
 
+app.use(errorLogger);
 app.use(errors());
 app.use(responseHandler);
 
